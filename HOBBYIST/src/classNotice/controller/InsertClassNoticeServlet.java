d9f7dd832c41daa24e4fc314c00f6bf917e90b17
Cho SangHeon <93089035+chosangheon@users.noreply.github.com>
2021-11-17 09:26:47 +0900

    1117 클래스 공지사항

--- HOBBYIST/src/classNotice/controller/InsertClassNoticeServlet.java
+++ HOBBYIST/src/classNotice/controller/InsertClassNoticeServlet.java
@@ -26,15 +26,6 @@ public class InsertClassNoticeServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		String classNoticeTitle = request.getParameter("classBoardName");
-		String classNoticeContent = request.getParameter("classBoardContent"); 
-
-		
-		String date = request.getParameter("date"); // date String값으로 넘어오는데 년도-월-일 이렇게 '-'로 구분돼서 넘어옴
-		 
-		Date classNoticeDate = null;
-		if(date.equals("")) { 
-			classNoticeDate = new Date(new GregorianCalendar().getTimeInMillis());
 		String classBoardName = request.getParameter("title");
 		String classBoardContent = request.getParameter("content"); 
 		String classNoticeWriter = ((Member)request.getSession().getAttribute("loginUser")).getMemberNickName();
@@ -51,16 +42,6 @@ public class InsertClassNoticeServlet extends HttpServlet {
 			int month = Integer.parseInt(splitDate[1]) - 1;
 			int day = Integer.parseInt(splitDate[2]);
 		
-			classNoticeDate = new Date(new GregorianCalendar(year, month, day).getTimeInMillis());
-		}
-		
-		ClassNotice n = new ClassNotice(0, classNoticeTitle, classNoticeContent, 0, classNoticeDate, 0, 0);
-		
-		
-		int result = new ClassNoticeService().insertNotice(n);
-
-		if (result > 0) {
-			response.sendRedirect("classNoticeInsert.no"); 
 			classBoardDate = new Date(new GregorianCalendar(year, month, day).getTimeInMillis());
 		}
 		
@@ -86,8 +67,4 @@ public class InsertClassNoticeServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b07ca7fe33222ce5421f3785ec1186dc42c1ff2c
