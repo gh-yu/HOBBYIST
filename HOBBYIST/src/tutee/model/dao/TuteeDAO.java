9bec055488dc4bd7ec4b6f20b9fe7da53448f4c7
gh-yu <yooumi99@gmail.com>
2021-11-25 00:57:28 +0900

    ....

--- HOBBYIST/src/tutee/model/dao/TuteeDAO.java
+++ HOBBYIST/src/tutee/model/dao/TuteeDAO.java
@@ -3,14 +3,20 @@ package tutee.model.dao;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
+
+import hobbyistClass.model.vo.HClass;
+
 import static common.JDBCTemplate.close;
 import tutee.model.vo.LikeClass;
+import tutee.model.vo.TuteeClass;
 import tutor.model.dao.TutorDAO;
 
 public class TuteeDAO {
@@ -106,6 +112,132 @@ public class TuteeDAO {
 		}
 		
 		return result;
+	}
+
+
+	public int insertTuteeClass(Connection conn, TuteeClass tc) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("insertTuteeClass");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setDate(1, tc.getTueeClassRevDate());
+			pstmt.setInt(2, tc.getClassNo());
+			pstmt.setString(3, tc.getMemberEmail());
+			pstmt.setString(4, tc.getTueeClassRevTime());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+
+	public int updateTuteeClassStatus(Connection conn) {
+		CallableStatement cstmt = null; // CallableStatement : 프로시저 호출을 위해 필요
+		int result = 0;
+		
+		String query = prop.getProperty("updateTuteeClassStatus");
+		
+		try {
+			cstmt = conn.prepareCall(query);
+			result = cstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(cstmt);
+		}
+		
+		return result;
+	}
+
+
+	public ArrayList<HClass> selectTuteeClassList(Connection conn, int i, String memberEmail) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<HClass> list = null;
+		
+		String query = null; 
+				
+		if (i == 0) {
+			query = prop.getProperty("selectTClassBeforeList");
+		} else {
+			query = prop.getProperty("selectTClassAfterList");
+		}
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberEmail);
+			rset = pstmt.executeQuery();
+			
+			list = new ArrayList<HClass>();
+			while(rset.next()) {
+				HClass hc = new HClass();
+				hc.setClassNo(rset.getInt("CLASS_NO"));
+				hc.setClassName(rset.getString("CLASS_NAME"));
+				hc.setClassStatus(rset.getString("CLASS_STATUS"));
+				
+				list.add(hc);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return list;
+	}
+
+
+	public ArrayList<TuteeClass> selectTCScheduleList(Connection conn, int i, String memberEmail) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<TuteeClass> list = null;
+		
+		String query = null; 
+				
+		if (i == 0) {
+			query = prop.getProperty("selectTcBeforeList");
+		} else {
+			query = prop.getProperty("selectTcAfterList");
+		}
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberEmail);
+			rset = pstmt.executeQuery();
+			
+			list = new ArrayList<TuteeClass>();
+			while(rset.next()) {
+				TuteeClass tc = new TuteeClass();
+				if (i == 0) {
+					tc.setClassNo(rset.getInt("CLASS_NO"));
+					tc.setTueeClassRevDate(rset.getDate("REV_DATE"));
+				} else {
+					tc.setClassNo(rset.getInt("CLASS_NO"));
+					tc.setTuteeClassFinishDate(rset.getDate("FINISH_DATE"));
+				}
+				
+				list.add(tc);
+			}			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return list;
 	}
 
 }
